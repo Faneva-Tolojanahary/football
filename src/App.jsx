da5fa2c6269ad7738fe5cbaeab4e87210de12908
Faneva-Tolojanahary <fanevarb560@gmail.com>
2025-04-25 10:06:51 +0200

    Update text

--- src/App.jsx
+++ src/App.jsx
@@ -59,8 +59,11 @@ const App = () => {
                 onChange={(e) => setSearch(e.target.value)}
               />
               <button
+                disabled={loading}
                 type="submit"
-                class="rounded-md bg-indigo-500 px-4 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                class={`rounded-md bg-indigo-500 px-4 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 ${
+                  loading ? "cursor-not-allowed" : "none"
+                }`}
               >
                 Rechercher
               </button>
